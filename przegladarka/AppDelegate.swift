ba26ce890e9b522c98b977de579e3073a1242e3e
Użytkownik Gość <Guest@imac-02.local>
2017-10-10 09:21:08 +0200

    xD

--- przegladarka/AppDelegate.swift
+++ przegladarka/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,9 +16,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+		var data = NSMutableData();
+		let internetURL = URL(string: "https://isebi.net/albums.php")
+		var request: NSURLRequest = NSURLRequest(url: internetURL!)
+		var connection: NSURLConnection = NSURLConnection(request: request as URLRequest, delegate: self, startImmediately: true)!
+		connection.start()
+		
         // Override point for customization after application launch.
         return true
     }
+		
+		
+	}
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
